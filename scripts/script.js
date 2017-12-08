41b03a7742a75b317bc0cbf963f9b6360ade5dce
elianahope12 <31940511+elianahope12@users.noreply.github.com>
2017-12-08 16:39:24 -0500

    week 3 commit

--- scripts/script.js
+++ scripts/script.js
@@ -4,51 +4,106 @@ $(document).ready(function() {
   var textnow;
   var visualnow;
   var counter = 0;
+  var intro_text = $("#textbox").html();
+
+  var female_singles = $("#female_singles");
+  var male_singles = $("#male_singles");
+  var pairs = $("#pairs");
+  var ice_dance = $("#ice_dance");
+  var last = $("#last");
+
 
 //when the button is clicked the intro hides
    $('#next').click(function() {
      counter++;
      switch (counter) {
-       case 1:
-        textnow = $("#female_singles");
+       case 2:
+        textnow = female_singles;
         $("#textbox").html(textnow);
         visualnow = $("<div id='female_chart'></div>");
         $("#visual").html(visualnow);
         makeFeChart();
         break;
-      case 2:
-       textnow = $("#male_singles");
+      case 3:
+       textnow = male_singles;
        $("#textbox").html(textnow);
        visualnow = $("<div id='male_chart'></div>");
        $("#visual").html(visualnow);
        makeMaChart();
        break;
-      case 3:
-       textnow = $("#pairs");
+      case 4:
+       textnow = pairs;
        $("#textbox").html(textnow);
        visualnow = $("<div id='pairs_chart'></div>");
        $("#visual").html(visualnow);
        makePaChart();
         break;
-      case 4:
-       textnow = $("#ice_dance");
+      case 5:
+       textnow = ice_dance;
        $("#textbox").html(textnow);
        visualnow = $("<div id='dancer_chart'></div>");
        $("#visual").html(visualnow);
        makeDaChart();
         break;
-      case 5:
-       textnow = $("#last");
-       $("#textbox").html(textnow);
-       visualnow = $("<div id='last_pics'></div>");
-       $("#visual").html(visualnow);
-       makeAllChart();
-        break;
-
+      case 6:
+         textnow = last;
+         $("#textbox").html(textnow);
+         visualnow = $('<img id="ashley" src="images/ashley.jpg" alt="Womens Singles Skater">');
+         $("#visual").html(visualnow);
+         break;
       } // end of switch
 
-    });
-});
+    }); /*  end of next button  */
+
+// start of back button
+$('#back').click(function() {
+   counter--;
+   if(counter <= 0){
+     counter = 0;
+   }
+   switch (counter) {
+     case 1:
+      textnow = intro_text; // this variable defined near top
+      $("#textbox").html(textnow);
+      visualnow = $('<img id="intro_pic" src="images/rink_large.png" alt="Womens Singles Skater">');
+      $("#visual").html(visualnow);
+      break;
+     case 2:
+      textnow = female_singles;
+      $("#textbox").html(textnow);
+      visualnow = $("<div id='female_chart'></div>");
+      $("#visual").html(visualnow);
+      makeFeChart();
+      break;
+     case 3:
+      textnow = male_singles;
+      $("#textbox").html(textnow);
+      visualnow = $("<div id='male_chart'></div>");
+      $("#visual").html(visualnow);
+      makeMaChart();
+      break;
+     case 4:
+      textnow = pairs;
+      $("#textbox").html(textnow);
+      visualnow = $("<div id='pairs_chart'></div>");
+      $("#visual").html(visualnow);
+      makePaChart();
+      break;
+     case 5:
+      textnow = ice_dance;
+      $("#textbox").html(textnow);
+      visualnow = $("<div id='dancer_chart'></div>");
+      $("#visual").html(visualnow);
+      makeDaChart();
+      break;
+     case 6:
+      textnow = last;
+      $("#textbox").html(textnow);
+      break;
+   }
+ }); // end of back button
+
+}); // end of document ready
 
 
 // everything below is Highcharts - do not touch
@@ -202,6 +257,3 @@ function makeDaChart() {
    });
 } //end of ice dance
 // end of charts
-function makeAllChart() {
-
-}
